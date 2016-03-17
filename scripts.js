5980e7edcd2a1582a20d8d90e792931eef6a2c0a
pwmurphy89 <pwmurphy89@gmail.com>
2016-03-17 15:48:31 -0400

    More updates

--- scripts.js
+++ scripts.js
@@ -5,69 +5,75 @@ $(document).ready(function() {
 	function keyCheck() {
 		var keyID = event.keyCode;
 		console.log(keyID);
+		var currScreenVal = $(".screen").val();
 		switch(keyID) {
 			case 49: {
-				$(".screen").val("1");
+				$(".screen").val(currScreenVal + "1");
 				break;
 			}
 			case 50: {
-				$(".screen").val("2");
+				$(".screen").val(currScreenVal+ "2");
 				break;
 			}
 			case 51: {
-				$(".screen").val("3");
+				$(".screen").val(currScreenVal + "3");
 				break;
 			}
 			case 52: {
-				$(".screen").val("4");
+				$(".screen").val(currScreenVal +"4");
 				break;
 			}
 			case 53: {
-				$(".screen").val("5");
+				$(".screen").val(currScreenVal + "5");
 				break;
 			}
 			case 54: {
-				$(".screen").val("6");
+				$(".screen").val(currScreenVal + "6");
 				break;
 			}
 			case 55: {
-				$(".screen").val("7");
+				$(".screen").val(currScreenVal + "7");
 				break;
 			}
 			case 56: {
-				$(".screen").val("8");
+				$(".screen").val(currScreenVal + "8");
 				break;
 			}
 			case 57: {
-				$(".screen").val("9");
+				$(".screen").val(currScreenVal + "9");
 				break;
 			}
 			case 61: {
-				$(".screen").val("+");
+				$(".screen").val(currScreenVal + "+");
 				break;
 			}
-			case 61: {
-				$(".screen").val("=");
+			case 187: {
+				doIt();
 				break;
 			}
-			case 45: {
-				$(".screen").val("-");
+			case 189: {
+				$(".screen").val(currScreenVal + "-");
 				break;
 			}
-			case 47: {
-				$(".screen").val("/");
+			case 191: {
+				$(".screen").val(currScreenVal + "/");
 				break;
 			}
 			case 48: {
-				$(".screen").val("0");
+				$(".screen").val(currScreenVal + "0");
 				break;
 			}
-			case 56: {
-				$(".screen").val("*");
+			case 88: {
+				$(".screen").val(currScreenVal + "*");
 				break;
 			}
-			case 61: {
-				$(".screen").val("=");
+			case 27: {
+				clearScreen();
+				break;
+			}
+			case 13: {
+				doIt();
+				
 				break;
 			}
 		}
@@ -75,3 +81,30 @@ $(document).ready(function() {
 
 
 })
+
+function doIt() {
+	try{
+	var total = eval($(".screen").val());
+
+}	catch(e){
+		console.log(e);
+		if (e instanceof SyntaxError){
+			alert("Error!");
+			return;
+		}else if(e instanceof ReferenceError){
+			alert("Error!");
+			return;
+		}
+	}
+	$(".screen").val(total);
+	
+}
+function insertVal(val) {
+	var currScreenVal = $(".screen").val();
+	$(".screen").val(currScreenVal + val);
+}
+
+function clearScreen() {
+	$(".screen").val("");
+
+}
